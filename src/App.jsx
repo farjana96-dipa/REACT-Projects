21acafe31c66de8e8182212957d82461c5867b20
farjana96-dipa <fparul32@gmail.com>
2025-02-08 22:47:59 +0600

    Second Commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Weather from './components/weather.jsx'
+import './index.css'
+
+const App = () => {
+  return (
+    <div className='app'>
+      <Weather></Weather>
+    </div>
+  )
+}
+
+export default App
